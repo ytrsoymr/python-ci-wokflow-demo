e018191e7944ab856f5e23e9a07f9cceca28c5a1
YARRAM THIRUPATHIRAO <138215815+ytrsoymr@users.noreply.github.com>
2025-04-18 12:29:00 +0530

    Create test_calculator.py

---
+++ tests/test_calculator.py
@@ -0,0 +1,10 @@
+import pytest
+from app.calculator import add, subtract
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+
+def test_subtract():
+    assert subtract(5, 2) == 3
+    assert subtract(10, 20) == -10
